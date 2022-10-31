a7c3b7b1660eec741c2068b1d5508cf650bf9c2d
William Fong <willfong@mit.edu>
2022-10-31 10:27:17 -0600

    adding high level export

--- pyticdb/__init__.py
+++ pyticdb/__init__.py
@@ -1,5 +1,7 @@
 """Top-level package for PyTICDB."""
+from .query import query_by_id, query_by_loc, query_by_str
 
+__all__ = ["query_by_id", "query_by_loc", "query_by_str"]
 __author__ = """William Fong"""
 __email__ = "willfong@mit.edu"
 __version__ = "0.1.0"
