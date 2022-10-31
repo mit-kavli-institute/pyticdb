801a51f3e57d8eb228cb161ae13d65833f7626ec
William Fong <willfong@mit.edu>
2022-10-31 11:11:09 -0600

    fixing import

--- pyticdb/__init__.py
+++ pyticdb/__init__.py
@@ -1,7 +1,7 @@
 """Top-level package for PyTICDB."""
-from .query import query_by_id, query_by_loc, query_by_str
+from .query import query_by_id, query_by_loc, query_raw
 
-__all__ = ["query_by_id", "query_by_loc", "query_by_str"]
+__all__ = ["query_by_id", "query_by_loc", "query_raw"]
 __author__ = """William Fong"""
 __email__ = "willfong@mit.edu"
 __version__ = "0.1.0"
