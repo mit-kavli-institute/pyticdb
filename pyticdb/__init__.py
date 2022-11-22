9e4033d9bca1541cda997664a149c33d844b0fae
William Fong <willfong@mit.edu>
2022-11-22 16:17:29 -0700

    bumping type

--- pyticdb/__init__.py
+++ pyticdb/__init__.py
@@ -4,4 +4,4 @@ from .query import query_by_id, query_by_loc, query_raw
 __all__ = ["query_by_id", "query_by_loc", "query_raw"]
 __author__ = """William Fong"""
 __email__ = "willfong@mit.edu"
-__version__ = "0.1.0"
+__version__ = "0.1.1"
