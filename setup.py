812e1964ad3bdb41b8aa755dbbd5a0ae0439e916
William Fong <willfong@mit.edu>
2023-04-18 10:33:28 -0600

    updating version

--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://tessgit.mit.edu/wcfong/pyticdb",
-    version="0.1.1",
+    version="0.1.2",
     zip_safe=False,
 )
