26a6ca38318ed083e4e6b3f1ed74cc602bf769d3
William Fong <willfong@mit.edu>
2023-04-18 11:33:49 -0600

    bumping version

--- pyticdb/__init__.py
+++ pyticdb/__init__.py
@@ -6,4 +6,4 @@ from .query import query_by_id, query_by_loc, query_raw
 __all__ = ["TicDB", "TICEntry", "query_by_id", "query_by_loc", "query_raw"]
 __author__ = """William Fong"""
 __email__ = "willfong@mit.edu"
-__version__ = "0.1.2"
+__version__ = "0.1.3"
--- setup.py
+++ setup.py
@@ -46,6 +46,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://tessgit.mit.edu/wcfong/pyticdb",
-    version="0.1.2",
+    version="0.1.3",
     zip_safe=False,
 )
